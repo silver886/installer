57d32f1e3d656035cef901cd63d70fe05b072895
Leo Liu <karta1041337@gmail.com>
2020-03-24 07:38:01 +0800

    Update error message

--- error.go
+++ error.go
@@ -4,15 +4,15 @@ import "errors"
 
 var (
 	// ErrStepNoDoer means the step does not have doer.
-	ErrStepNoDoer = errors.New("No doer")
+	ErrStepNoDoer = errors.New("Step has no doer")
 	// ErrStepNoUndoer means the step does not have undoer.
-	ErrStepNoUndoer = errors.New("No undoer")
+	ErrStepNoUndoer = errors.New("Step has no undoer")
 	// ErrStepNonDone means the step had not been done.
-	ErrStepNonDone = errors.New("Non done")
+	ErrStepNonDone = errors.New("Step is not done")
 	// ErrStepNonUndone means the step had not been undone.
-	ErrStepNonUndone = errors.New("Non undone")
+	ErrStepNonUndone = errors.New("Step is not undone")
 	// ErrStepDone means the step had already done.
-	ErrStepDone = errors.New("Already done")
+	ErrStepDone = errors.New("Step is already done")
 	// ErrStepUndone means the step had already undone.
-	ErrStepUndone = errors.New("Already undone")
+	ErrStepUndone = errors.New("Step is already undone")
 )
